54e999c84c6a7a651cc85610e54c3848bd3085d1
Geovanni <carbajal.perez180320@gmail.com>
2025-01-29 12:33:21 -0600

    Iniciacion de Backend

---
+++ backend-go/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Ruta de prueba
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	r.Run(":8080") // Ejecuta el servidor en el puerto 8080
+}
